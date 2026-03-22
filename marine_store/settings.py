bd349c41cb4706787128628d421714157a590a72
Mente prabhu kumar <prabhu.m@mushroom.solutions>
2026-03-22 09:29:36 +0530

    render build requirements

--- marine_store/settings.py
+++ marine_store/settings.py
@@ -69,9 +69,9 @@ INSTALLED_APPS = [
 
 
 cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME', 'dvucztr6n'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY','335212166476688'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET','euoyvLFEqeGfOubwQ0I9WqTiI8I'),
     secure=True,  # force https URLs
 )
 
